31cc09ec598fb4c2c318cf01ea7a4a02be12c60b
yellownine <aronov.tor@gmail.com>
2020-08-31 14:28:55 +0300

    add helper method doesn't work

--- test/src/tests/composing.ts
+++ test/src/tests/composing.ts
@@ -194,7 +194,8 @@ import { Suit, SuitHelper,
          that as msThat,
          xthat as msXThat,
          after as msAfter,
-         afterEach as msAfterEach, } from 'mocha-suit-ts';
+         afterEach as msAfterEach,
+         withHelper} from 'mocha-suit-ts';
 
 import sinon = require("sinon");
 
@@ -252,10 +253,10 @@ describe(MSG, function(){
 
             before(RunSpyMethods);
 
-            it(msMethod.name+" should be run twice",function(){
+            it(msMethod.name+" method should be run twice",function(){
                 TestingMethods[msMethod.name].calledTimes().should.be.eql(2);
             });
-            it("helper method should be called", function() {
+            it(msMethod.name+" helper method should be called", function() {
                 expect(helperSpy.called).to.be.ok();
             });
 
@@ -310,16 +311,100 @@ describe(MSG, function(){
 
             before(RunSpyMethods);
 
-            it(msMethod.name+" should be run triple",function(){
+            it(msMethod.name+" method should be run triple",function(){
                 if (/^x/.test(msMethod.name)) {
                     TestingMethods.xit.calledTimes().should.be.eql(3);
                 } else {
                     TestingMethods.it.calledTimes().should.be.eql(3);
                 };
+            });
+            it(msMethod.name+" helper method should be called", function(){
                 expect(helperSpy.called).to.be.ok();
             });
 
             after(ResetSpyMethods);
         });
-    })
+    });
+
+    describe("withHelper", function() {
+        let beforeSpy = sinon.spy();
+        let beforeEachSpy = sinon.spy();
+        let itSpy = sinon.spy();
+        let xitSpy = sinon.spy();
+        let thatSpy = sinon.spy();
+        let xthatSpy = sinon.spy();
+        let afterEachSpy = sinon.spy();
+        let afterSpy = sinon.spy();
+
+        before(function() {
+            @SuitHelper()
+            class Helper{
+                @msBefore()
+                beforeFunction() { return beforeSpy(); }
+
+                @msBeforeEach()
+                beforeEachFunction() { return beforeEachSpy(); }
+
+                @msIt()
+                itFunction() { return itSpy(); }
+
+                @msXIt()
+                xitFunction() { return xitSpy(); }
+
+                @msThat()
+                thatFunction() {return thatSpy(); }
+
+                @msXThat()
+                xthatFunction() { return xthatSpy(); }
+
+                @msAfterEach()
+                afterEachFunction() { return afterEachSpy(); }
+
+                @msAfter()
+                afterFunction() { return afterSpy(); }
+            };
+
+            @Suit()
+            class SuperSuit {
+                @msBefore()
+                beforeFunction() {};
+
+                @msBeforeEach()
+                beforeEachFunction() {};
+
+                @msIt()
+                itFunction() {};
+
+                @msXIt()
+                xitFunction() {};
+
+                @msThat()
+                thatFunction() {};
+
+                @msXThat()
+                xthatFunction() {};
+
+                @msAfterEach()
+                afterEachFunction() {};
+
+                @msAfter()
+                afterFunction() {};
+
+                @withHelper(Helper)
+                helperProp: any;
+            };
+
+            new SuperSuit()
+        });
+
+        before(RunSpyMethods);
+
+        [beforeSpy, beforeEachSpy, itSpy, xitSpy, thatSpy, xthatSpy, afterEachSpy, afterSpy].forEach(function(spyMethod) {
+            it(spyMethod.name + " helper method should be called",function(){
+                expect(spyMethod.called).to.be.ok();
+            });
+
+        after(ResetSpyMethods);
+        });
+    });
 });
