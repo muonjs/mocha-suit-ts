71c4a06bf4abfa3d348cd9945b85102c62a754d7
yellownine <aronov.tor@gmail.com>
2020-09-02 10:11:40 +0300

    first test done

--- test/src/tests/context.ts
+++ test/src/tests/context.ts
@@ -4,27 +4,26 @@ import { RunSpyMethods } from '../setup';
 import { ResetSpyMethods } from '../setup';
 
 import { Suit,
-         it as msIt} from "../../../src/app";
+         it as msIt} from "mocha-suit-ts";
 
 const MSG = "Context generation.";
 
 describe(MSG,function(){
 
-    describe("With generator call.",function(){
+    describe("With generator call.",function(){  //подумать над правильным описанием теста
+        let testCtx = {
+            testKey: "test_context"
+        };
         before(function(){
-            this.testCtx = {
-                testKey: true
-            };
-
             const self = this;
 
             @Suit("some")
             class SuperSuit {
-                msTSctx: any;
+                testKey: string;
 
                 constructor(testCtx: any) {
-                    this.msTSctx = testCtx;
-                };
+                    this.testKey = testCtx.testKey;
+                }
 
                 @msIt()
                 itFunction() {
@@ -32,145 +31,145 @@ describe(MSG,function(){
                 };
             };
 
-            new SuperSuit(this.testCtx);
+            new SuperSuit(testCtx);
         });
 
         before(RunSpyMethods);
 
-        it("testKey should exists",function() {
-            this.retCtx.should.be.eql(this.testCtx);
+        it("testKey should exists and has wright value",function() {
+            this.retCtx.should.be.eql(testCtx);
         });
 
         after(ResetSpyMethods);
     });
 
-    // // describe("With extend call.",function(){
-    // //     before(function(){
-    // //         this.testCtx = {
-    // //             testKey: true
-    // //         };
-    // //         this.suit = mod("some").extend("",this.testCtx);
-    // //
-    // //         const self = this;
-    // //
-    // //         this.suit.it("",function(){
-    // //             self.retCtx = this.suit;
-    // //         });
-    // //
-    // //         this.suit();
-    // //     });
-    // //
-    // //     before(RunSpyMethods);
-    // //
-    // //     it("testKey should exists",function() {
-    // //         this.retCtx.should.be.eql(this.testCtx);
-    // //     });
-    // //
-    // //     after(ResetSpyMethods);
-    // // });
-    // //
-    // // describe("With test run call.",function(){
-    // //     before(function(){
-    // //         this.testCtx = {
-    // //             testKey: true
-    // //         };
-    // //         this.suit = mod("some");
-    // //
-    // //         const self = this;
-    // //
-    // //         this.suit.it("",function(){
-    // //             self.retCtx = this.suit;
-    // //         });
-    // //
-    // //         this.suit(this.testCtx);
-    // //     });
-    // //
-    // //     before(RunSpyMethods);
-    // //
-    // //     it("testKey should exists",function() {
-    // //         this.retCtx.should.be.eql(this.testCtx);
-    // //     });
-    // //
-    // //     after(ResetSpyMethods);
-    // // });
-    // //
-    // // describe("Extend should rewrite lower extend.",function(){
-    // //     before(function(){
-    // //         this.suit = mod("some").extend("",{
-    // //             testKey: 1
-    // //         }).extend("",{
-    // //             testKey: 2
-    // //         });
-    // //
-    // //         const self = this;
-    // //
-    // //         this.suit.it("",function(){
-    // //             self.retCtx = this.suit;
-    // //         });
-    // //
-    // //         this.suit();
-    // //     });
-    // //
-    // //     before(RunSpyMethods);
-    // //
-    // //     it("testKey should have last set value",function() {
-    // //         this.retCtx.should.have.property("testKey",2);
-    // //     });
-    // //
-    // //     after(ResetSpyMethods);
-    // // });
-    // //
-    // // describe("Test call ctx should rewrite value set within extend call.",function(){
-    // //     before(function(){
-    // //         this.suit = mod("some").extend("",{
-    // //             testKey: 1
-    // //         });
-    // //
-    // //         const self = this;
-    // //
-    // //         this.suit.it("",function(){
-    // //             self.retCtx = this.suit;
-    // //         });
-    // //
-    // //         this.suit({
-    // //             testKey: 2
-    // //         });
-    // //     });
-    // //
-    // //     before(RunSpyMethods);
-    // //
-    // //     it("testKey should have last set value",function() {
-    // //         this.retCtx.should.have.property("testKey",2);
-    // //     });
-    // //
-    // //     after(ResetSpyMethods);
-    // // });
-    // //
-    // // describe("Any before method call with ctx changing should rewrite passed ctx.",function(){
-    // //     before(function(){
-    // //         this.suit = mod("some");
-    // //
-    // //         const self = this;
-    // //
-    // //         this.suit.before(function(){
-    // //             this.suit.testKey = 2;
-    // //         });
-    // //
-    // //         this.suit.it("",function(){
-    // //             self.retCtx = this.suit;
-    // //         });
-    // //
-    // //         this.suit({
-    // //             testKey: 1
-    // //         });
-    // //     });
-    // //
-    // //     before(RunSpyMethods);
-    // //
-    // //     it("testKey should have last set value",function() {
-    // //         this.retCtx.should.have.property("testKey",2);
-    // //     });
-    // //
-    // //     after(ResetSpyMethods);
+    // describe("With extend call.",function(){
+    //     before(function(){
+    //         this.testCtx = {
+    //             testKey: true
+    //         };
+    //         this.suit = mod("some").extend("",this.testCtx);
+    //
+    //         const self = this;
+    //
+    //         this.suit.it("",function(){
+    //             self.retCtx = this.suit;
+    //         });
+    //
+    //         this.suit();
+    //     });
+    //
+    //     before(RunSpyMethods);
+    //
+    //     it("testKey should exists",function() {
+    //         this.retCtx.should.be.eql(this.testCtx);
+    //     });
+    //
+    //     after(ResetSpyMethods);
+    // });
+    //
+    // describe("With test run call.",function(){
+    //     before(function(){
+    //         this.testCtx = {
+    //             testKey: true
+    //         };
+    //         this.suit = mod("some");
+    //
+    //         const self = this;
+    //
+    //         this.suit.it("",function(){
+    //             self.retCtx = this.suit;
+    //         });
+    //
+    //         this.suit(this.testCtx);
+    //     });
+    //
+    //     before(RunSpyMethods);
+    //
+    //     it("testKey should exists",function() {
+    //         this.retCtx.should.be.eql(this.testCtx);
+    //     });
+    //
+    //     after(ResetSpyMethods);
+    // });
+    //
+    // describe("Extend should rewrite lower extend.",function(){
+    //     before(function(){
+    //         this.suit = mod("some").extend("",{
+    //             testKey: 1
+    //         }).extend("",{
+    //             testKey: 2
+    //         });
+    //
+    //         const self = this;
+    //
+    //         this.suit.it("",function(){
+    //             self.retCtx = this.suit;
+    //         });
+    //
+    //         this.suit();
+    //     });
+    //
+    //     before(RunSpyMethods);
+    //
+    //     it("testKey should have last set value",function() {
+    //         this.retCtx.should.have.property("testKey",2);
+    //     });
+    //
+    //     after(ResetSpyMethods);
+    // });
+    //
+    // describe("Test call ctx should rewrite value set within extend call.",function(){
+    //     before(function(){
+    //         this.suit = mod("some").extend("",{
+    //             testKey: 1
+    //         });
+    //
+    //         const self = this;
+    //
+    //         this.suit.it("",function(){
+    //             self.retCtx = this.suit;
+    //         });
+    //
+    //         this.suit({
+    //             testKey: 2
+    //         });
+    //     });
+    //
+    //     before(RunSpyMethods);
+    //
+    //     it("testKey should have last set value",function() {
+    //         this.retCtx.should.have.property("testKey",2);
+    //     });
+    //
+    //     after(ResetSpyMethods);
+    // });
+    //
+    // describe("Any before method call with ctx changing should rewrite passed ctx.",function(){
+    //     before(function(){
+    //         this.suit = mod("some");
+    //
+    //         const self = this;
+    //
+    //         this.suit.before(function(){
+    //             this.suit.testKey = 2;
+    //         });
+    //
+    //         this.suit.it("",function(){
+    //             self.retCtx = this.suit;
+    //         });
+    //
+    //         this.suit({
+    //             testKey: 1
+    //         });
+    //     });
+    //
+    //     before(RunSpyMethods);
+    //
+    //     it("testKey should have last set value",function() {
+    //         this.retCtx.should.have.property("testKey",2);
+    //     });
+    //
+    //     after(ResetSpyMethods);
     // });
 });
